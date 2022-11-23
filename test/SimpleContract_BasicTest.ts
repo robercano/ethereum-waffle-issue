f881c2e60ea4d45b2878d9fb72533447a9fec81c
robercano <3525807+robercano@users.noreply.github.com>
2022-11-23 10:07:49 +0100

    feat: try to reproduce the issue (fail)

--- test/SimpleContract_BasicTest.ts
+++ test/SimpleContract_BasicTest.ts
@@ -1,10 +1,7 @@
-import chai, { expect } from "chai";
-import chaiEthersBN from "chai-ethers-bn";
+import { expect } from "chai";
 import { ethers } from "hardhat";
 
 import { SimpleContract } from "../typechain";
-
-chai.use(chaiEthersBN());
 
 describe("SimpleContract", function () {
     describe("Basic Cases", function () {
@@ -16,24 +13,8 @@ describe("SimpleContract", function () {
             simpleContract = (await SimpleContract.deploy()) as SimpleContract;
             await simpleContract.deployed();
         });
-        it("Valid deployment", async function () {
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(0);
-        });
-        it("Increment", async function () {
-            await simpleContract.increment();
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(1);
-            await simpleContract.increment();
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(2);
-            await simpleContract.increment();
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(3);
-        });
-        it("IncrementBy", async function () {
-            await simpleContract.incrementBy(8);
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(8);
-            await simpleContract.incrementBy(5);
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(13);
-            await simpleContract.incrementBy(9);
-            await expect(await simpleContract.value()).to.be.a.bignumber.that.equals(22);
+        it("Always fails", async function () {
+            await expect(simpleContract.fail()).to.be.revertedWith("This function always fails");
         });
     });
 });
